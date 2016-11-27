c27a93e6367a4039cf7797ec822b4ee5543420f6
daniel-reich <Daniel@Daniels-MacBook-Pro-4.local>
2016-11-26 17:14:00 -0700

    ReadWrite

---
+++ TextReadWrite/src/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+        public static void main(String[] args) {
+            Path myFilePath = Paths.get("TextFileReadWrite.txt");
+            String myFileContent = "We are writing and reading a text file.";
+
+            // writing the text file
+            try {
+                BufferedWriter writer = Files.newBufferedWriter(myFilePath);
+                writer.write(myFileContent);
+                writer.flush();
+                writer.close();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+
+            // reading the text file
+            try {
+                BufferedReader reader = Files.newBufferedReader(myFilePath);
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+
+    }
+
